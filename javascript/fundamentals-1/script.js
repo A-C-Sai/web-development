c0c6a37ba7cb13fbfedf0c12fc5a119377877eff
Sai C Akella <sai.c.akella@gmail.com>
2024-06-22 22:19:45 +1000

    javascript

--- javascript/fundamentals-1/script.js
+++ javascript/fundamentals-1/script.js
@@ -1,1 +1,43 @@
-alert("Hello World!");
+/** Value - smallest unit of information we can have in javascript
+
+console.log(23);
+console.log("Hello World!");
+*/
+
+
+/** Variables
+ * variables names can only contain alphanumeric, _ and $
+ * cannot start with a number (alphabets, _ and $ acceptable)
+ * reserved keywords not allowed
+ * variable that are all in UPPER CASE are reserved for conastants
+
+let firstName; // camelCase naming convention
+firstName = "Sam";
+console.log(firstName);
+ */ 
+
+
+/** Data Types
+ * every value is javascript is either an Object or a Primitive 
+ * In javascript we don't have to manually define the data type of the value stored in a variable.
+ 
+console.log(typeof(23)); // Number
+console.log(typeof(30.5)); // Number
+console.log(typeof("Hello World")); // String
+console.log(typeof(true)); // Boolean
+console.log(typeof(false)); // Boolean
+
+let uninitializedVariable;
+console.log(uninitializedVariable); // Undefined
+console.log(typeof(uninitializedVariable)); // Undefined
+
+console.log(typeof(null)); // Object (bug)
+*/
+
+
+/** Declaring Variables
+ * const 
+ * let
+ * var
+ */
+
