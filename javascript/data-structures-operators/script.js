818bb4a257341d3eafec598c73a1c7911da1c0db
Sai C Akella <sai.c.akella@gmail.com>
2024-06-29 04:19:39 +1000

    Destructuring Arrays

---
+++ javascript/data-structures-operators/script.js
@@ -0,0 +1,24 @@
+// 'use strict';
+
+/* Destructuring Arrays
+const arr = [1, 2, 3];
+const [a, b, c] = arr; // simple destructuring
+console.log(a, b, c);
+const [d, , e] = arr; // skipping elements
+console.log(d, e);
+
+let f = 2;
+let g = 3;
+console.log(f, g);
+[f, g] = [g, f]; // switching variables without temp variable
+console.log(f, g);
+
+const nested = [1, 2, [3, 4]]; 
+const [h, , [i, j]] = nested; // destructuring within destructuring
+console.log(h, i, j);
+
+const [k, l, m] = [8, 9];
+console.log(k, l, m);
+const [n=1, o=1, p=1] = [8, 9]; // default values
+console.log(k, l, m);
+*/
