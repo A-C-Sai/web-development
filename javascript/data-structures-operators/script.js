49c6b453cb55cd4f79ad5c8319de40d9f09c5f7f
Sai C Akella <sai.c.akella@gmail.com>
2024-07-08 05:46:42 +1000

    destructuring objects

--- javascript/data-structures-operators/script.js
+++ javascript/data-structures-operators/script.js
@@ -1,4 +1,4 @@
-// 'use strict';
+'use strict';
 
 /* Destructuring Arrays
 const arr = [1, 2, 3];
@@ -22,3 +22,52 @@ console.log(k, l, m);
 const [n=1, o=1, p=1] = [8, 9]; // default values
 console.log(k, l, m);
 */
+
+/* Destructuring Objects
+const {property-1, property-2, ..., property-n} = Object;
+const {property-1: aliasName, property-2: aliasName, ..., property-n: aliasName} = Object;
+
+-> can set default values to properties that may not exist [] (empty array)
+
+
+// mutating variables
+let a = 111;
+let b = 234;
+const obj = { a: 6, b: 32 };
+console.log(a, b);
+({ a, b } = obj);
+console.log(a, b);
+
+
+// nested objects
+const nestedObj = {
+    fullName: {
+        firstName: "Harry",
+        lastName: "Potter"
+    }
+};
+
+const { fullName: { firstName: fName, lastName: lName = "" } } = nestedObj;
+console.log(fName, lName);
+
+
+// practical use
+const obj = {
+    greeting: function ({ firstName, lastName = "" }) {
+        console.log(`Greetings ${firstName} ${lastName}`);
+    }
+}
+
+const person1 = {
+    lastName: "Potter",
+    firstName: "Harry"
+}
+
+obj.greeting(person1);
+*/
+
+
+
+
+
+
