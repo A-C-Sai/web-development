c7a86d90cfbc567ea341642c4ad65a8f71962d7f
Sai C Akella <sai.c.akella@gmail.com>
2024-10-21 22:33:20 +0530

    javascript

---
+++ javascript/working-with-arrays/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/**
+ * Methods are simply functions that we can call on objects.
+ * .slice() without any arguments can be used to create a shallow copy of an array.
+ * .splice(start_index, deleteCount) will modify the original array unlike .slice(). The extracted elements are removed from the original array.
+ * .reverse() mutates the original array
+ * .concat()
+ * .at() --> also works on strings
+ * .forEach(function(currentValue, index, arr){stmt...})
+ * Continue And Break don't work inside foEach
+ * forEach also works on maps and sets, .forEach(function(value, key, map)) & .forEach(function(value, _, set))
+ */
