5e284083181821ea3d0e8f8758f9e9eec42fd88f
Sai C Akella <sai.c.akella@gmail.com>
2025-06-09 22:55:17 +1000

    js

--- javascript/data-structures-operators/script.js
+++ javascript/data-structures-operators/script.js
@@ -4,6 +4,7 @@
 const arr = [1, 2, 3];
 const [a, b, c] = arr; // simple destructuring
 console.log(a, b, c);
+const [a_, b_] = arr; // not all elements need to be taken out, here were are only taking out the first two.
 const [d, , e] = arr; // skipping elements
 console.log(d, e);
 
@@ -17,8 +18,8 @@ const nested = [1, 2, [3, 4]];
 const [h, , [i, j]] = nested; // destructuring within destructuring
 console.log(h, i, j);
 
-const [k, l, m] = [8, 9];
-console.log(k, l, m);
+const [k, l, m] = [8, 9]; 
+console.log(k, l, m); // m will be undefined
 const [n=1, o=1, p=1] = [8, 9]; // default values
 console.log(k, l, m);
 */
@@ -29,7 +30,7 @@ In the above example the variable names have to match the original property name
 
 const {property-1: aliasName, property-2: aliasName, ..., property-n: aliasName} = Object;
 
--> can set default values to properties that may not exist [] (empty array)
+-> can set default values to properties that may not exist [] (empty array) otherwise they will have value of undefined
 
 const {property-1: aliasName, property-2: aliasName = []} = Object;
 
@@ -39,8 +40,8 @@ let a = 111;
 let b = 234;
 const obj = { a: 6, b: 32 };
 console.log(a, b);
-({ a, b } = obj);
-console.log(a, b);
+({ a, b } = obj); ---> wrapping everything in parantheses
+console.log(a, b); ----> a = 6, b = 32 we have over-written the initial values
 
 
 // nested objects
@@ -70,8 +71,8 @@ obj.greeting(person1);
 */
 
 /* Spread Operator
-  -> works on all iterables (strings, maps and arrays) NOT objects.
-  -> can use it where we usually pass multiple values separated by commas.
+  -> works on all iterables (strings, maps and arrays) NOT objects (objects are not iterables).
+  -> can use it where we usually pass multiple values separated by commas. ------> IMPORTANT e.g. can't use in template literals / strings
   -> can use it to expand arrays/ objects based on existing arrays/ objects.
 
 const arr = [1, 2, 3];
@@ -109,7 +110,7 @@ console.table(person1);
 console.table(person2);
 */
 
-/* Rest Pattern and Parameters
+/* Rest Pattern and Parameters (think of this as packing, and spread operator as unpacking)
 // rest pattern always must be the last in the destructuring assignment. AND there can only be 1 rest pattern per destructuring assignment. Works with objects also.
 const [a, , b, ...others] = [1, 2, 3, 4, 5, 6];
 console.log(a, b, others);
@@ -144,13 +145,17 @@ multiply(1, 2, 3, 4);
 
 const params = [5, 7, 8, 9];
 multiply(...params); // here it is the spread operator
+
+function myFunc(firstParam, ...otherParams){
+  stmt....
+}
 */
 
 /* 
   -> Logical Operators can use any data type, can return any data type, short-circuit evaluation.
   -> || returns the first truthy value else the last element (edge case: when the value is 0)
   -> && returns the first falsey value else the last element
-  -> can use the short-circuiting behaviour in place of ternery operator and if statements
+  -> can use the short-circuiting behaviour in place of ternery operator (|| to set default values) and if statements ( && Run second operand if the first value exists)
 
   const obj = {
   Fname: "John",
@@ -168,22 +173,22 @@ console.log(obj.Lname); // undefined
   time: "4:12",
   place: "Hall",
 };
-const guests = party.numberOfguests || "Not Mentioned";
-const guests = party.numberOfguests ?? "Not Mentioned";
+const guests = party.numberOfguests || "Not Mentioned"; ----> even if numberOfguests exists and has a value of 0 the default value is set
+const guests = party.numberOfguests ?? "Not Mentioned";  -----> this solves the problem
 
 console.log(guests);
 
  */
 
 /* Logical Assignment Operator
- * ||=, ??=,
+ * ||=, ??=, -----> assign a value if / to already false/ nullish value
  * &&= -----> assign a value to a already truthy value
  */
 
-/** for-of loop
- * for (const <varname> of iterable) stmt...
+/** for-of loop                                 _________ Array Iterator
+ * for (const <varname> of iterable) stmt...    |
  * for (const [index, <varname>] of iterable.entries()) stmt...
- * console.log([...iterable.entries()]);
+ * console.log([...iterable.entries()]); ---> Array or Arrays
  */
 
 /** Enhanced Object Literals
@@ -191,11 +196,11 @@ console.log(guests);
 const Lname = "Smith";
 const person = {
   Fname: "John",
-  Lname,
-  fullName() {
+  Lname, -----> defined outside obj
+  fullName() { ----> no need for property : func expression
     console.log(`${this.Fname} ${this.Lname}.`);
   },
-  [`${1 + 3}th month sales`]: 400,
+  [`${1 + 3}th month sales`]: 400, ----> computing property names
 };
 
 */
@@ -209,10 +214,12 @@ const person = {
 
 /** Looping Objects
  *
- * Object.keys(<object>)
+ * Object.keys(<object>) ---> array with property names
  * Object.values(<object>)
- * Object.entries(<object>)
+ * Object.entries(<object>) ----> destructure into key, value
  *
+ *
+ * We are able to access non-existing array indexes --> undefined; we can also set values for non-consecutive indexes.
  */
 
 /** Sets
@@ -220,58 +227,115 @@ const person = {
  * unique elements
  * order is not maintained, cannot use index so we cant retrieve elements, we can just check it's existence
  * can have an empty set; const s = new Set();
- * .size
+ * .size -----> instead of .length
  * .has(<element>)
  * .add(<element>)
  * .delete(<element>)
  * .clear()
- * sets are also iterables
- * can use the spread operator
+ * sets are also iterables ----> can loop over them
+ * can use the spread operator ----> [...set]
+ *
+ * set1.intersections(set2) ---> returns new set
+ * set1.union(set2)
+ * set1.difference(set2) ---> order of sets matter
+ * set1.symmetricDifference(set2)
+ *
+ * we also have methods for isSubsetOf, isSupersetOf and isDisjointFrom
+ *
+ *
  */
 
 /** Maps
- * In objects, the keys can only be strings but Maps supports a wide variety of data types for keys. e.g. array and objects can be used as keys, remember to use the same reference while setting property and retrieving as arrays and objects reside in heap.
+ * In objects, the keys can only be strings BUT Maps supports a wide variety of data types for keys. e.g. array and objects can be used as keys, remember to use the same reference while setting property and retrieving as arrays and objects reside in heap.
  * const m = new Map();
  * .set(<key>, <value>) ---> returns the modified map which allows us to chain multiple set commands
  * .get(<key>) --> data type of key is important, if key doesn't exist, we get undefined
- * .has(<key>)
- * .delete(<key>)
+ * .has(<key>) ---> objects also have similar method
+ * .delete(<key>) -------> can also delete properties from Objs using delete operator but its a slow process
  * .size
  * .clear()
- * const m2 = new Map([[<key>, <vale>],[<key>, <vale>],[<key>, <vale>]...,[<key>, <vale>]])
+ * const m2 = new Map([[<key>, <vale>],[<key>, <vale>],[<key>, <vale>]...,[<key>, <vale>]]) ---> array of arrays
  * Object to Map conversion: new Map(Object.entries(<object>))
- * Maps are iterables, OBJECTS ARE NOT
- * Maps to Array: used spread operator on Map
- * .keys()
- * .values()
- * .entries()
+ * Maps are iterables, OBJECTS ARE NOT ---> for (const [key, val] of map) stmt...
+ 
+ * Maps to Array: used spread operator on Map ---> [...map] ---> array of arrays
+ * .keys()  ----> Map Iterator so use spread operator
+ * .values() ----> Map Iterator so use spread operator
+ * .entries() ----> Map Iterator so use spread operator  ---> same as [...map]
+ */
+
+// There are also other built-in and non built-in Data Structures in JS.
+
+/** Array vs Set vs Obj vs Map
+ *
+ * Array
+ *  - need ordered list of values allowing duplicates
+ *  -when we need to manipulate data
+ *
+ *
+ * Set
+ *  - need to work with unique values
+ *  - high performance is imp
+ *  - remove duplicates from array
+ *
+ * Obj
+ *  - traditional key/value store
+ *  - easier to write and access
+ *  -use when we need to include functions (methods)
+ *  - when working with JSON (can convert to map)
+ *
+ *
+ * Map
+ *  - performance
+ *  - keys any datatype
+ *  - easy to iterate
+ *  - easy to compute size
+ *  - when we simple need to map key to values
+ *  - need keys that are not strings
+ *
+ *
  */
 
 /** Strings (primitives) (cannot modify)
+ 
  * can index, but negative index not possible
- * .length
- * .indexOf("<element>/<substring>") ---> first occurrence
+ * .length ---> var.length or 'string'.length
+ * .indexOf("<element>/<substring>") ---> first occurrence or -1
  * .lastIndexOf("<element>")
- * .slice(begin, end) [begin, end) ; can use negative numbers
- * strings are primitives but they have methods. JS automatically converts string primitive to sting object using new String() when a method is called. i.e. Boxing
- * all string method return string  primitive.
+ * .slice(begin)   
+   .slice(begin, end) [begin, end)
+   can use negative numbers and mix of +ve n -ve ---> length = end - begin
+ 
+ * strings are primitives but they have methods. JS automatically converts string primitive to sting object with the same content using new String(), it is this obj that are methods are called upon. When the operation is done, th eobj is converted back to a regular string. All string methods return primitives, even if called on a string obj i.e. Boxing
+
+
  * .toLowerCase()
  * .toUpperCase()
- * .trim()
- * can chain methods
- * .replace("<to_replace>","<replace_with>") ---> replaces first appearance; case sensitive
+ * .trim(), .trimStart(), .trimEnd()
+ * CAN CHAIN METHODS
+ * .replace("<to_replace>","<replace_with>") ---> replaces first appearance; CASE SENSITIVE like all string methods are
  * .replaceAll()
+
+
  * can use RegEx
+
  * .includes("<substring>")
  * .startsWith("<substring>")
  * .endsWith("<substring>")
+ 
  * .split("<delimiter>") --> returns array
  * <arrayObj>.join("<delimiter>")
+
+
  * .padStart(total_length, pad_with)
  * .padEnd(total_length, pad_with)
- * String() --> type casting
- * number + '' = string
+
+
+ * String() --> type casting OR number + '' = string
  * .repeat(number_of_times)
+ * 
+ * 
+ * \ escaping
  */
 
 /* Challenge
@@ -362,6 +426,13 @@ const game = {
 // for (const [team, odds] of Object.entries(game.odds)) {
 //   console.log(
 //     `Odd of ${game[team] ? `victory ${game[team]}:` : `draw:`} ${odds}`
+//   );
+// }
+
+
+// for (const [team, odds] of Object.entries(game.odds)) {
+//   console.log(
+//     `Odd of ${team === 'x' ? `draw` : `victory ${game[team]}`}: ${odds}`
 //   );
 // }
 
